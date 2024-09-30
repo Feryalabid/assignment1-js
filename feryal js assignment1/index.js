fac13f7a7873a236a63e4a1c3936d360d4eb7ddb
Feryalabid <feryalabid123456@gmail.com>
2024-10-01 00:36:36 +0500

    Add files via upload

---
+++ feryal js assignment1/index.js
@@ -0,0 +1,42 @@
+//Q no 2 of ch 1
+alert("Welcome to my website!");
+//Q no 4 of ch 1
+alert("Hello, world!")
+//Q no 5 of ch 1
+alert("This is an example.");
+//Q no 1 of ch 2
+var message = "Hello, world!";
+alert(message);
+//Q no 2 of ch 2
+var age;
+age = 25;
+alert(age);
+//Q no 3 of ch 2
+var greeting = "Good morning!";
+alert(greeting);
+var greeting = "Good evening!";
+alert(greeting);
+//Q no 4 of ch 2
+var favoriteColor = "blue";
+alert("My favorite color is "  + favoriteColor);
+//Q no 5 of ch 2 is same as Q no 2 of this ch.
+
+//Q no 1 of ch 3
+var height = 170;
+var newheight = height + 10;
+alert(newheight);
+//Q no 2 of ch 3
+var num1 = 50;
+var num2 = 30;
+var sum = num1 + num2;
+alert(sum);
+//Q no 3 of ch 3
+var numToBeAdded = 20;
+var num = 300;
+var total = 320;
+alert(total);
+//Q no 4 of ch 3
+var price = 120;
+var tax = price * 0.10;
+var  totalprice = price + tax;
+alert(totalprice);
